8e509ce039228950d9c1cedc17497748b03e978f
Dumitru Cristian <78100242+CrisAxe@users.noreply.github.com>
2026-03-02 22:01:05 +0000

    addfeat:add index.json

--- session4/src/services/database.py
+++ session4/src/services/database.py
@@ -1,8 +1,25 @@
+import json
+from pathlib import Path
 from data.models import Meeting
 from uuid import uuid4
+
+
+BASE_PATH = Path ("saves")
+INDEX_PATH = Path("saves/index.json")
 
 def create(meeting:Meeting):
     BASE_PATH = "saves"
     filename = f"{BASE_PATH}/{uuid4()}.md"
     with open(filename,"w") as file:
         file.writelines(str(meeting))
+
+    
+    if not INDEX_PATH.exists():
+        INDEX_PATH.touch([])
+
+    index_content = None
+    breakpoint()
+    with open(INDEX_PATH.absolut(),"r") as file:
+        
+        index_content = json.loads(file.read())
+        
