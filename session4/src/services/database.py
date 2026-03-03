ea8ce00025eaa67ef0f6446bd921546ba53f59a3
Dumitru Cristian <78100242+CrisAxe@users.noreply.github.com>
2026-03-03 21:31:39 +0000

    fix:correção de erro

--- session4/src/services/database.py
+++ session4/src/services/database.py
@@ -1,6 +1,8 @@
+from dataclasses import asdict
 import json
 from pathlib import Path
-from data.models import Meeting
+
+from data.models import Meeting,MeetingMetadata
 from uuid import uuid4
 
 
@@ -8,18 +10,38 @@ BASE_PATH = Path ("saves")
 INDEX_PATH = Path("saves/index.json")
 
 def create(meeting:Meeting):
-    BASE_PATH = "saves"
     filename = f"{BASE_PATH}/{uuid4()}.md"
     with open(filename,"w") as file:
         file.writelines(str(meeting))
 
     
     if not INDEX_PATH.exists():
-        INDEX_PATH.touch([])
+        INDEX_PATH.touch()
+        INDEX_PATH.write_text("[]")
 
     index_content = None
-    breakpoint()
-    with open(INDEX_PATH.absolut(),"r") as file:
-        
-        index_content = json.loads(file.read())
+
+    with open(INDEX_PATH.absolute(),"r") as file:
+        index_content:list = json.loads(file.read())
+
+    index_content.append(
+        asdict(MeetingMetadata(
+            meeting=meeting,
+            path=filename
+        ))
+    )
+    
+    with open(INDEX_PATH,"w") as file:
+        json.dump(index_content,file)
+    # index = []
+    # for meeting in index_content:
+    #     new_mm = MeetingMetadata(
+    #         meeting=Meeting(
+    #             title=meeting["meeting"]["title"]
+    #             owner=meeting["meeting"]["owner"]
+    #             date=meeting["meeting"]["date"]
+    #         ),
+    #         path=meeting["path"]
+    #     )
+    #     index.append(new_mm)
         
